274fe74b213c9d8e258d6357895d3201942bc377
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2017-05-04 15:09:09 -0600

    Nuevos cambios en el programa

---
+++ lab11.s
@@ -0,0 +1,161 @@
+/* Integrantes: 
+Ivette Cardona, 16020 
+Mercedes Retolaza, 16339 
+Laboratorio no.11 
+Descripción del programa: Se encienden tres leds de manera intermitente.;*/ 
+
+@ puertosES_2.s prueba con puertos de entrada y salida
+@ Funciona con cualquier puerto, utilizando biblioteca gpio0_2.s
+	
+  .text
+ .align 2
+ .global main
+ 
+main:
+	@utilizando la biblioteca GPIO (gpio0.s)
+	bl GetGpioAddress 	@solo se llama una vez
+	
+    /* Se cambian los GPIO de escritura por los puertos 17,18 y 27  */
+	@GPIO para escritura (salida) puerto 17
+	mov r0,#17
+	mov r1,#1 
+	bl SetGpioFunction
+    /* Comienzan cambios */ 
+    /* Se añaden puertos de escritura para los leds  */
+    @GPIO para escritura (salida) puerto 18
+	mov r0,#18
+	mov r1,#1
+	bl SetGpioFunction
+
+	@GPIO para escritura (salida) puerto 27
+	mov r0,#27
+	mov r1,#1
+	bl SetGpioFunction
+	/* El puerto de lectura se fija en el GPIO 14 */ 
+   	@GPIO para lectura (entrada) puerto 14 
+	mov r0,#14
+	mov r1,#0
+	bl SetGpioFunction
+	bl wait @ Se añade una sub rutina de espera para que se puedan observar los cambios en los leds 
+
+	/* Se incorpora instruccion de apagar */ 
+	@Apagar GPIO 18
+	mov r0,#18
+	mov r1,#0
+	bl SetGpio
+
+	/* Se incorpora instruccion de apagar */ 
+	@Apagar GPIO 27
+	mov r0,#27
+	mov r1,#0
+	bl SetGpio
+
+	bl wait @ se añade instrucción de pausa 
+
+loop:
+	/*@Revision del boton
+	@Para revisar si el nivel de un GPIO esta en alto o bajo se revisa 
+	@se lee en la direccion 0x20200034 para los GPIO 0-31*/ 
+
+	ldr r6, =myloc
+ 	ldr r0, [r6] 		@ obtener direccion de la memoria virtual 
+	ldr r5,[r0,#0x34] 	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
+	mov r7,#1
+	lsl r7,#14
+	and r7,r5,r7 		@se revisa el bit 14 (puerto de entrada)
+
+	/* Se añaden cambios*/ 
+	@Si el boton esta en alto (1), fue presionado y enciende GPIO 17
+	teq r7,#0
+	beq  revision1 
+	b  loop
+
+	/* Se añaden etiquetas en el programa, esto con el fin de hacer mas sencilla su interacion y movimientos */ 
+revision1:
+	ldr r6, =myloc
+ 	ldr r0, [r6] 		@ obtener direccion de la memoria virtual 
+	ldr r5,[r0,#0x34] 	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
+	mov r7,#1
+	lsl r7,#17
+	and r5,r7,r7 		@se revisa el bit 17 (puerto de salida)
+	
+	teq  r5,#0
+	beq fin1 @se llama a la etiqueta fin1 
+	bne revision2 
+	b  loop 
+
+revision2: 
+	ldr r6, =myloc
+ 	ldr r0, [r6] 		@ obtener direccion de la memoria virtual 
+	ldr r5,[r0,#0x34] 	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
+	mov r7,#1
+	lsl r7,#18
+	and r5,r7,r7 @se revisa el bit 18 (puerto de salida)
+	
+	@Si el boton esta en alto (1), fue presionado y enciende GPIO 18
+	teq r7,#0
+	beq fin2 @se llama a la etiqueta fin2
+	bne revision3
+	b  loop 
+
+revision3: 
+	ldr r6, =myloc
+ 	ldr r0, [r6] 		@ obtener direccion de la memoria virtual 
+	ldr r5,[r0,#0x34] 	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
+	mov r7,#1
+	lsl r7,#27
+	and r5,r7,r5		@se revisa el bit 27 (puerto de salida)
+	@Si el boton esta en alto (1), fue presionado y enciende GPIO 27
+
+	teq  r7,#0
+	beq fin3 @se llama a la etiqueta fin3
+	bne revision4
+	b   loop 
+
+/*revision4
+
+	ldr r6, =myloc
+ 	ldr r0, [r6] 		@ obtener direccion de la memoria virtual 
+	ldr r5,[r0,#0x34] 	@Direccion r0+0x34:lee en r5 estado de puertos de entrada
+	mov r7,#1
+	lsl r7,#27
+	and r5,r7,r5		@se revisa el bit 27 (puerto de salida)
+	@Si el boton esta en alto (1), fue presionado y enciende GPIO 27
+
+	teq  r7,#0
+	beq fin3 @se llama a la etiqueta fin3
+	bne revision4
+	b   loop */ 
+
+/* Se realizan las instrucciones de salida del sistema*/ 
+
+fin1: 
+	movne r0,#17 
+	movne r1, #1 
+	blne SetGpio 
+	b loop 
+fin2: 
+	movne r0,#18 
+	movne r1, #1 
+	blne SetGpio 
+	b loop 
+fin3: 
+	movne r0,#27 
+	movne r1, #1 
+	blne SetGpio 
+	b loop 
+
+@ brief pause routine
+ wait:
+ mov r0, #0x4000000 @ big number
+ sleepLoop:
+ subs r0,#1
+ bne sleepLoop @ loop delay
+ mov pc,lr
+
+ .data
+ .align 2
+.global myloc
+myloc: .word 0
+
+ .end
